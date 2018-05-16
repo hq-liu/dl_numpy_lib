262d2196bd1e254c389f881f4ee5fe647c63b2a6
lhq1208 <1972573764@qq.com>
2018-05-16 14:29:17 +0800

    update

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from PIL import Image
 import numpy as np
 
 
-ckpt = open('two_hidden_layer_epoch_5.pkl', 'rb')
+ckpt = open('two_hidden_layer_epoch_5.pkl', 'rb')  # 读取训练好的参数，我大概训练了一下，没有细致调参
 model = pickle.load(ckpt)['model']
 ckpt.close()
 
 img_root = './data_utils/cole'
 while True:
     """
-    主循环，实际运行时在这个里面运行，其他的只是与训练有关，与主程序无关
+    主循环，实际运行时在这个里面运行，其他的只是与训练有关，与主程序无关，
+    想要训练的话在fc_net_train.py里
+    我只用了两层的fc，也可以用cnn，但是估计是因为样本的原因，训练结果上不去了
     """
     while sum([len(x) for _, _, x in os.walk(img_root)]) < 10:
         """
